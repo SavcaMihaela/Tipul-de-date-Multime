722d508a2a5e5d7d19bd5e40267e74fbcf26a4e4
Savca Mihaela <70644060+SavcaMihaela@users.noreply.github.com>
2021-11-08 11:39:44 +0300

    Add files via upload

---
+++ pb.8 pag.60.py
@@ -0,0 +1,10 @@
+A={1,2 ,3}
+B={9,2,3,4}
+print(A)
+print(B)
+##a
+print('Intersectia multimii A si B', A.intersection(B))
+##b
+print('Reuniunea multimii A si B', A.union(B))
+##c
+print('Diferenta multimii A si B', A.difference(B))
